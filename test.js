8839239421b669a7e03b117053d97431c023e34d
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-03 22:46:50 +0200

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,12 +1,11 @@
-'use strict';
-var execFile = require('child_process').execFile;
-var path = require('path');
-var test = require('ava');
+import {execFile} from 'child_process';
+import path from 'path';
+import test from 'ava';
 
-test(function (t) {
+test(t => {
 	t.plan(2);
 
-	execFile(path.join(__dirname, 'cli.js'), ['--help'], function (error, stdout) {
+	execFile(path.join(__dirname, 'cli.js'), ['--help'], (error, stdout) => {
 		t.error(error);
 		t.regexTest(/GitHub repo exists/, stdout);
 	});
